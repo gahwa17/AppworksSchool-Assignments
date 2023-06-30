6f65dc41a645e7e04c45512be049a360811fac6a
gahwa17 <s24527109@gmail.com>
2023-07-01 00:22:45 +0800

    Upload assignment5

--- Week-1/Assignment-5/app.js
+++ Week-1/Assignment-5/app.js
@@ -3,7 +3,32 @@ const app = express();
 const port = 3000;
 
 app.get('/data', (req, res) => {
-
+  let number = req.query.number;
+  if(!number) {
+    res.send('Lack of Parameter');
+  }
+  else {
+    number = parseInt(number);
+    if(Number.isNaN(number)) {
+      res.send('Wrong Parameter');
+    }
+    else {
+      /* Note
+      console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
+      console.log(Number.MAX_); // 1.7976931348623157e+308
+      Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2 // true
+      */
+      
+      // Check if the number exceeds the safe calculation range
+      if(number >= Number.MAX_SAFE_INTEGER){
+        res.send('Number exceeds the safe calculation range');
+      }
+      else{
+        let sum = (1 + number) * number / 2;
+        res.send(`The sum of (1+2+....+${number}) is ${sum}`);
+      }
+    }
+  }
 });
 
 app.listen(port, () => {
